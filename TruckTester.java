521624c1811ec1af2a2aa010a0536e9169084c5f
smihallari <smihallari@york.citycollege.eu>
2025-01-10 11:38:27 +0200

    blabla

---
+++ TruckTester.java
@@ -0,0 +1,19 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+public class TruckTester {
+    
+    
+        @Test
+        public void testAdd() {
+            
+            assertEquals(1, );
+        }
+    
+        @Test
+        public void testRemove() {
+            
+            assertEquals(1);
+        }
+    }
+    
+
